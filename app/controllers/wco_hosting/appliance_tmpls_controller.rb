8c9bf806a5a443bfb9d0a6e1c277391e42d45715
mac_a2141 <victor@piousbox.com>
2023-12-23 18:52:05 -0500

    autobuild `date "+%Y%m%d"`

--- app/controllers/wco_hosting/appliance_tmpls_controller.rb
+++ app/controllers/wco_hosting/appliance_tmpls_controller.rb
@@ -1,7 +1,57 @@
 
 class WcoHosting::ApplianceTmplsController < WcoHosting::ApplicationController
+
+  before_action :set_lists
+
+  def create
+    @appliance_tmpl = WcoHosting::ApplianceTmpl.new params[:appliance].permit!
+    authorize! :create, @appliance_tmpl
+
+    flag = @appliance_tmpl.save
+    if flag
+      flash[:notice] = 'Success.'
+      redirect_to action: :index
+    else
+      flash[:alert] = "Cannot create appliance tmplate: #{@appliance_tmpl.errors.full_messages.join(', ')}."
+      redirect_to action: :index
+    end
+  end
+
+  def edit
+    @appliance_tmpl = WcoHosting::ApplianceTmpl.find params[:id]
+    authorize! :edit, @appliance_tmpl
+  end
+
   def index
     authorize! :index, WcoHosting::ApplianceTmpl
     @appliance_tmpls = WcoHosting::ApplianceTmpl.all
   end
+
+  def new
+    authorize! :index, WcoHosting::ApplianceTmpl
+    @appliance_tmpl = WcoHosting::ApplianceTmpl.new
+  end
+
+  def update
+    @appliance_tmpl = WcoHosting::ApplianceTmpl.find params[:id]
+    authorize! :update, @appliance_tmpl
+    flag = @appliance_tmpl.update params[:appliance].permit!
+    if flag
+      flash_notice 'success'
+    else
+      flash_alert "Cannot update appliance template: #{@appliance_tmpl.errors.full_messages.join(', ')}."
+    end
+    redirect_to action: :index
+  end
+
+  ##
+  ## private
+  ##
+  private
+
+  def set_lists
+    @new_appliance_tmpl = WcoHosting::ApplianceTmpl.new
+  end
+
 end
+
