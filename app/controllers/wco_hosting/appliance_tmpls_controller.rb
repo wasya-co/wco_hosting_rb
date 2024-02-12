efdf1cd6dbc9d38c18e697ca7edec109a8fadcfd
mac_a2141 <victor@piousbox.com>
2024-02-11 23:14:32 -0600

    groking deploying react frontend

--- app/controllers/wco_hosting/appliance_tmpls_controller.rb
+++ app/controllers/wco_hosting/appliance_tmpls_controller.rb
@@ -4,6 +4,8 @@ class WcoHosting::ApplianceTmplsController < WcoHosting::ApplicationController
   before_action :set_lists
 
   def create
+    params[:tmpl][:leadset_ids].delete ''
+
     @appliance_tmpl = WcoHosting::ApplianceTmpl.new params[:tmpl].permit!
     authorize! :create, @appliance_tmpl
 
@@ -15,6 +17,18 @@ class WcoHosting::ApplianceTmplsController < WcoHosting::ApplicationController
       flash[:alert] = "Cannot create appliance tmplate: #{@appliance_tmpl.errors.full_messages.join(', ')}."
       redirect_to action: :index
     end
+  end
+
+  def destroy
+    @appliance_tmpl = WcoHosting::ApplianceTmpl.find params[:id]
+    authorize! :destroy, @appliance_tmpl
+    flag = @appliance_tmpl.delete
+    if flag
+      flash_notice 'ok'
+    else
+      flash_alert @appliance_tmpl
+    end
+    redirect_to action: 'index'
   end
 
   def edit
@@ -33,15 +47,17 @@ class WcoHosting::ApplianceTmplsController < WcoHosting::ApplicationController
   end
 
   def update
+    params[:tmpl][:leadset_ids].delete ''
+
     @appliance_tmpl = WcoHosting::ApplianceTmpl.find params[:id]
     authorize! :update, @appliance_tmpl
 
-    price = Wco::Price.find( params[:tmpl][:price] )
-    price.product = @appliance_tmpl
-    price.save
+    # price = Wco::Price.find( params[:tmpl][:price] )
+    # price.product = @appliance_tmpl
+    # price.save
 
-    params[:tmpl][:price_id] = price.price_id
-    params[:tmpl][:price] = price
+    # params[:tmpl][:price_id] = price.price_id
+    # params[:tmpl][:price] = price
 
     flag = @appliance_tmpl.update params[:tmpl].permit!
     if flag
@@ -58,8 +74,10 @@ class WcoHosting::ApplianceTmplsController < WcoHosting::ApplicationController
   private
 
   def set_lists
-    # @prices_list        = Wco::Price.list
+    @leadsets      = Wco::Leadset.all
+    @leadsets_list = Wco::Leadset.list
     @new_appliance_tmpl = WcoHosting::ApplianceTmpl.new
+    # @profiles_list = Wco::Profile.list
   end
 
 end
