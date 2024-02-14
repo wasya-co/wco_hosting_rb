77d642bff2b38f8e4674aacdc0b3a63ee9a19832
mac_a2141 <victor@piousbox.com>
2024-02-14 18:26:21 -0500

    autobuild `date "+%Y%m%d"`

--- test/dummy/spec/controllers/wco_hosting/appliance_tmpls_controller_spec.rb
+++ test/dummy/spec/controllers/wco_hosting/appliance_tmpls_controller_spec.rb
@@ -9,6 +9,17 @@ RSpec::describe WcoHosting::ApplianceTmplsController do
     @tmpl = create( :appliance_tmpl )
   end
 
+  it '#create' do
+    n = WcoHosting::ApplianceTmpl.all.count
+    post :create, params: { tmpl: {
+      kind: 'a',
+      version: '0.0.0',
+      image: 'b',
+      volume_zip: 'c',
+    } }
+    WcoHosting::ApplianceTmpl.all.count.should eql( n + 1 )
+  end
+
   it '#edit' do
     get :edit, params: { id: @tmpl.id }
     response.code.should eql '200'
