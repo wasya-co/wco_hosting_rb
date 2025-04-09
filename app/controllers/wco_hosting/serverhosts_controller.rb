5f5802f6e8fb2d2b37eec22234e82345a54c81b5
mac_a2141 <victor@piousbox.com>
2025-04-09 18:50:57 -0500

    can almost deploy an smt6

--- app/controllers/wco_hosting/serverhosts_controller.rb
+++ app/controllers/wco_hosting/serverhosts_controller.rb
@@ -5,6 +5,8 @@ class WcoHosting::ServerhostsController < WcoHosting::ApplicationController
     @serverhost = WcoHosting::Serverhost.new params[:serverhost].permit!
     authorize! :create, @serverhost
 
+    @serverhost.do_create_server! unless params[:skip_do_resource]
+
     flag = @serverhost.save
     if flag
       flash[:notice] = 'Success.'
@@ -15,6 +17,23 @@ class WcoHosting::ServerhostsController < WcoHosting::ApplicationController
     end
   end
 
+  def destroy
+    @serverhost = WcoHosting::Serverhost.find params[:id]
+    authorize! :destroy, @serverhost
+    out = HTTParty.delete( "https://api.digitalocean.com/v2/droplets/#{@serverhost.do_id}",
+      headers: { 'Authorization' => "Bearer #{DO_READER_TOKEN}" },
+        :debug_output => $stdout,
+      );
+    puts! out, 'out'
+    flag = @serverhost.destroy!
+    if flag
+      flash_notice @serverhost
+    else
+      flash_alert @serverhost
+    end
+    redirect_to action: :index
+  end
+
   def edit
     @serverhost = WcoHosting::Serverhost.find params[:id]
     authorize! :edit, @serverhost
@@ -22,7 +41,7 @@ class WcoHosting::ServerhostsController < WcoHosting::ApplicationController
 
   def index
     authorize! :index, WcoHosting::Serverhost
-    @serverhosts = WcoHosting::Serverhost.all
+    @serverhosts = WcoHosting::Serverhost.all.order_by name: :asc
     @new_serverhost = WcoHosting::Serverhost.new
   end
 
